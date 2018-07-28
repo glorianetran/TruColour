e0e018cc67d2a42b05a51cfbddf2bd7fa3ce0d7f
Nadine Bui <nadine.bui@zaplabs.com>
2018-07-28 13:25:19 -0700

    slash screen done

--- app/src/main/java/com/spectra/glodine/trucolour/SplashActivity.java
+++ app/src/main/java/com/spectra/glodine/trucolour/SplashActivity.java
@@ -1,14 +1,44 @@
 package com.spectra.glodine.trucolour;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
 }
